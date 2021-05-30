313feab2b3ad5541edbc8eb0f1acc55e76f337c0
Ren Xiang <foorenxiang@gmail.com>
2021-05-30 12:22:48 +0800

    added offline pwa for updates notes app

---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/notes/expo-service-worker.js",{scope:"/notes/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
